4c1e381f300277b272534f3efffd0660001e105a
Dimas Wisnu Nugroho <dimaswn777@gmail.com>
2020-02-25 16:15:58 +0700

    'Update Login.php'

--- Login.php
+++ Login.php
@@ -11,4 +11,29 @@ class Login extends CI_Controller
     {
         $this->load->view('vw_login');
     }
+
+    public function act_login()
+    {
+        $ckUser = $this->ml->check_data($email, $password);
+
+        if($ckUser->num_rows() > 0)
+        {
+            foreach($ckUser->result() as $dt)
+            {
+                $sessData = [
+                    'user_id'   => $dt->user_id,
+                    'email'     => $dt->email,
+                    'hak_akses' => $dt->hak_akses
+                ];
+            }
+
+            $this->session->set_userdata($sessData);
+
+            echo "Login Success";
+        }
+        else
+        {
+            echo "Login Failed";
+        }
+    }
 }
